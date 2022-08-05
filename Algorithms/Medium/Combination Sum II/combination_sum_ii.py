d9c4fbe97f8a8c68dca8d6cedcac06feacb08c17
JadKHaddad <jadkhaddad@gmail.com>
2022-08-05 11:32:38 +0200

    Combination Sum II update

--- Algorithms/Medium/Combination Sum II/combination_sum_ii.py
+++ Algorithms/Medium/Combination Sum II/combination_sum_ii.py
@@ -4,7 +4,7 @@ from typing import List
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
-        combinations = set()
+        combinations = []
 
         def c(index, target, subCombinations):
             for i in range(index, len(candidates)):
@@ -12,15 +12,15 @@ class Solution:
                     continue
                 if candidates[i] >= target:
                     if candidates[i] == target:
-                        combinations.add(
-                            tuple(subCombinations + [candidates[i]]))
+                        combinations.append(
+                            subCombinations + [candidates[i]])
                     break
 
                 c(i+1, target - candidates[i],
                   [candidates[i]] + subCombinations)
 
         c(0, target, [])
-        return [list(x) for x in combinations]
+        return combinations
 
 
 candidates = [1, 1, 2, 5, 6, 7, 10]
